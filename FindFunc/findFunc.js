3575a460f7ef4d5ca4f92b6bc71507fbf0f5e0d1
Cierra-McDonald <cierralauren15@gmail.com>
2021-03-23 23:50:42 -0700

    fakeReduce works past test

--- FindFunc/findFunc.js
+++ FindFunc/findFunc.js
@@ -3,7 +3,7 @@ const fakeFind = (arr, callback) => {
 
     for (let i = 0; i < arr.length; i++) { 
         arr[i] = callback(arr[i]);
-        
+
         if( arr[i] === true) { 
             return i;
         }
--- ReduceFunc/reduceFunc.js
+++ ReduceFunc/reduceFunc.js
@@ -1,6 +1,13 @@
-const fakeReduce = () => { 
 
-    return
+
+const fakeReduce = (arr, callback) => { 
+    let empObj = {};
+    for(let i = 0; i < arr.length; i++) { 
+
+        empObj = callback(empObj, arr[i]);
+        
+    }
+    return empObj;
 }
 
 module.exports = fakeReduce;
